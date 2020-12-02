e241f19902b5718aeb752540027725e5b3c8cf35
wajihov <wajihov@hotmail.fr>
2020-12-02 22:51:20 +0100

    Ajouter les components, les services, guardS et les interfaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './Auth/login/login.component';
+import { RegisterComponent } from './Auth/register/register.component';
+import { ShopComponent } from './shop/shop.component';
+import { ProductsComponent } from './shop/products/products.component';
+import { SingleProductComponent } from './shop/single-product/single-product.component';
+import { CartComponent } from './shop/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ContactComponent,
+    LoginComponent,
+    RegisterComponent,
+    ShopComponent,
+    ProductsComponent,
+    SingleProductComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule
